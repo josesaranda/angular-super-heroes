12f6af448fe6d2448d4345be55b7597d16314357
josesaranda <josesaranda@gmail.com>
2023-03-31 18:03:42 +0200

    improvements

--- src/components/delete-dialog/delete-dialog.component.ts
+++ src/components/delete-dialog/delete-dialog.component.ts
@@ -4,7 +4,6 @@ import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
 @Component({
   selector: "app-delete-dialog",
   templateUrl: "./delete-dialog.component.html",
-  styleUrls: ["./delete-dialog.component.scss"],
 })
 export class DeleteDialogComponent {
   name: string;
--- src/services/heroes-data-access.service.ts
+++ src/services/heroes-data-access.service.ts
@@ -1,4 +1,3 @@
-import { Injectable } from "@angular/core";
 import { Observable, of } from "rxjs";
 import { v4 } from "uuid";
 import { Hero } from "./hero.interface";
